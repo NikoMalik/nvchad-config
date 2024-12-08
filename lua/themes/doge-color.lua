666d669412d8356450d61264884cd9db31c1163d
NikoMalik <123812634+NikoMalik@users.noreply.github.com>
2024-12-08 13:51:47 +0300

    new theme mono

--- lua/themes/doge-color.lua
+++ lua/themes/doge-color.lua
@@ -5,24 +5,24 @@ local M = {}
 -- UI
 M.base_30 = {
     white = "#ebdab4",
-    black = "#0f1115", -- background
-    darker_black = "#0c0d10", -- 6% darker than black
-    black2 = "#181a1e", -- can adjust based on design
-    one_bg = "#2e2e2e", -- 10% lighter than black
-    one_bg2 = "#3e3e37", -- 6% lighter than one_bg
-    one_bg3 = "#504a43", -- 6% lighter than one_bg2
-    grey = "#7a7168", -- 40% lighter than black
-    grey_fg = "#938b32", -- 10% lighter than grey
-    grey_fg2 = "#a89e94", -- 5% lighter than grey
-    light_grey = "#cbbca3",
+    black = "#14161b", -- background
+    darker_black = "#14161b", -- 6% darker than black
+    black2 = "#1d2021", -- can adjust based on design
+    one_bg = "#3c3836", -- 10% lighter than black
+    one_bg2 = "#504945", -- 6% lighter than one_bg
+    one_bg3 = "#665c54", -- 6% lighter than one_bg2
+    grey = "#928374", -- 40% lighter than black
+    grey_fg = "#a89984", -- 10% lighter than grey
+    grey_fg2 = "#bdae93", -- 5% lighter than grey
+    light_grey = "#d5c4a1",
     red = "#fb4934",
     baby_pink = "#d3869b",
     pink = "#b16286",
-    line = "#202226", -- 15% lighter than black
+    line = "#282828", -- 15% lighter than black
     green = "#32c885",
     vibrant_green = "#b8bb26",
+    nord_blue = "#458588",
     blue = "#83a598",
-    nord_blue = "#458589",
     seablue = "#8ec07c",
     yellow = "#d79921", -- 8% lighter than yellow
     sun = "#fabd2f",
@@ -31,20 +31,20 @@ M.base_30 = {
     teal = "#689d6a",
     orange = "#d65d0e",
     cyan = "#689d6a",
-    statusline_bg = "#2e2e2e",
-    lightbg = "#403b36",
-    pmenu_bg = "#37575c",
-    folder_bg = "#d4c4a2",
+    statusline_bg = "#3c3836",
+    lightbg = "#504945",
+    pmenu_bg = "#458588",
+    folder_bg = "#ebdab4",
 }
 
 M.base_16 = {
-    base00 = "#0f1115",
+    base00 = "#14161b",
     base01 = "#ebdab4",
-    base02 = "#403b36",
-    base03 = "#504a43",
+    base02 = "#504945",
+    base03 = "#665c54",
     base04 = "#bdae93",
-    base05 = "#a89e94",
-    base06 = "#cbbca3",
+    base05 = "#d5c4a1",
+    base06 = "#ebdbb2",
     base07 = "#C0C0C0",
     base08 = "#cc241d",
     base09 = "#d79921",
@@ -53,7 +53,7 @@ M.base_16 = {
     base0C = "#83a598",
     base0D = "#458588",
     base0E = "#b16286",
-    base0F = "#cbbca3",
+    base0F = "#ebdbb2",
 }
 
 -- M.add = {
@@ -75,20 +75,17 @@ M.polish_hl = {
         -- ["*"] = { fg = M.base_30.green },
         ["@keyword.import.c"] = { fg = M.base_30.green },
         ["@function"] = { fg = M.base_30.white },
-        ["@function.make"] = { fg = "#4d4f50" },
-        ["@function.builtin"] = { fg = "#4d4f50" },
+        ["@function.make"] = { fg = M.base_30.red },
+        ["@function.builtin"] = { fg = M.base_30.baby_pink },
         ["@comment"] = { fg = M.base_30.cyan, italic = true },
         ["@module"] = { fg = M.base_30.white },
         ["@variable"] = { fg = M.base_30.white },
-        ["@keyword"] = { fg = M.base_30.white },
-        ["@keyword.directive.define.c"] = { fg = M.base_30.green },
+        ["@keyword"] = { fg = M.base_30.red },
         ["@keyword.import.go"] = { fg = M.base_30.green },
         ["@keyword.type.struct"] = { fg = M.base_30.nord_blue },
-        ["@keyword.type"] = { fg = M.base_30.white },
-        ["@keyword.repeat"] = { fg = "#e8e8e8" },
-        ["@keyword.operator.c"] = { fg = "#73b6f1" },
+        ["@keyword.repeat"] = { fg = M.base_30.red },
         ["@type"] = { fg = M.base_30.white },
-        ["@type.builtin.c"] = { fg = "#e8e8e8" },
+        ["@type.builtin.c"] = { fg = M.base_30.red },
         ["@punctuation.separator"] = { fg = M.base_30.green },
         ["@property"] = { fg = M.base_30.white },
 
@@ -96,11 +93,11 @@ M.polish_hl = {
         ["@variable.member"] = { fg = M.base_30.white },
         ["@variable.parameter"] = { fg = M.base_30.white },
         ["@attribute"] = { fg = M.base_30.white },
-        ["@keyword.function"] = { fg = M.base_30.white },
-        ["@function.call"] = { fg = "#b3af9e" },
-        ["@keyword.return"] = { fg = "#4d4f50" },
-        ["@keyword.conditional"] = { fg = "#e8e8e8" },
-        ["@function.method.call"] = { fg = M.base_30.white },
+        ["@keyword.function"] = { fg = M.base_30.red },
+        ["@function.call"] = { fg = M.base_30.baby_pink },
+        ["@keyword.return"] = { fg = M.base_30.red },
+        ["@keyword.conditional"] = { fg = M.base_30.red },
+        ["@function.method.call"] = { fg = M.base_30.baby_pink },
         ["@function.method"] = { fg = M.base_30.white },
         ["@function.builtin.lua"] = { fg = M.base_30.red },
         ["@operator"] = { fg = "#73b6f1" },
@@ -111,7 +108,6 @@ M.polish_hl = {
         ["@type.definition"] = { fg = M.base_30.white },
         ["@number"] = { fg = M.base_30.baby_pink },
         ["@operand"] = { fg = M.base_30.green },
-        ["@string"] = { fg = "#c2b9a7" },
 
         -- [""]
     },
